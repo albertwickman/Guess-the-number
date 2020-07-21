d66540c712fac502c589c846b778c306d8cfd41f
magnetixaft <Liseberg20!>
2020-07-21 22:11:18 +0200

    Alberts

---
+++ GuessTheNumber.py
@@ -0,0 +1,48 @@
+from random import randrange
+from math import fabs
+
+
+def tryNumber(user):
+    while True:
+        try:
+            n = int(input(user + " bet: "))
+        except ValueError:
+            pass
+        else:
+            if n in range(0, 101):
+                return n
+
+
+def computerGuess():
+    randomGuess = randrange(0, 101)
+    return randomGuess
+
+
+def main():
+    user1Guess = tryNumber("User 1")
+    user2Guess = tryNumber("User2")
+
+    randompicked = computerGuess()
+
+    u1diff = fabs(randompicked - user1Guess)
+    u2diff = fabs(randompicked - user2Guess)
+
+    valid_answer_yes = "Yes"
+    valid_answer_no = "No"
+
+    if u1diff == u2diff:
+        print("Tie")
+    elif u1diff < u2diff:
+        print("User 1 won")
+    else:
+        print("User 2 won")
+
+    redo = input("Do you want to play again? ")
+    if redo in valid_answer_yes:
+        main()
+    elif redo in valid_answer_no:
+        print("The game has been terminated...")
+        exit()
+
+
+main()
